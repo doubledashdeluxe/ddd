27b58d6f6bdc49a91727705f88dd4adf947a0dcf
Pablo Stebler <pablo@stebler.xyz>
2023-10-19 23:58:01 +0200

    Add missing shift in GXInitTlutObj patch

--- payload/dolphin/GXTexture.cc
+++ payload/dolphin/GXTexture.cc
@@ -6,6 +6,6 @@ extern "C" void GXInitTlutObj(GXTlutObj *tlut_obj, void *lut, s32 fmt, u16 n_ent
     tlut_obj->fmt = fmt << 10;
     // The Dolphin SDK doesn't reset the middle bits, which is an issue in Wii mode as they are not
     // masked. Reference (Dolphin Emulator): https://is.gd/ghHrLe
-    tlut_obj->lut = 0x64 | (reinterpret_cast<u32>(lut) & 0x1ffffff) >> 5;
+    tlut_obj->lut = 0x64 << 24 | (reinterpret_cast<u32>(lut) & 0x1ffffff) >> 5;
     tlut_obj->n_entries = n_entries;
 }
