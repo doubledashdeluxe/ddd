2ca50d6f2b37c4e230dd516c8523996c7cc20478
Pablo Stebler <pablo@stebler.xyz>
2023-11-12 16:41:22 +0100

    Allow textures to be loaded into MEM2

--- payload/dolphin/GXTexture.cc
+++ payload/dolphin/GXTexture.cc
@@ -2,10 +2,16 @@ extern "C" {
 #include "GXTexture.h"
 }
 
+extern "C" void GXInitTexObj(GXTexObj *obj, void *image_ptr, u16 width, u16 height, s32 format,
+        s32 wrap_s, s32 wrap_t, BOOL mipmap) {
+    REPLACED(GXInitTexObj)(obj, image_ptr, width, height, format, wrap_s, wrap_t, mipmap);
+    obj->image_ptr = (reinterpret_cast<uintptr_t>(image_ptr) & 0x13ffffff) >> 5;
+}
+
 extern "C" void GXInitTlutObj(GXTlutObj *tlut_obj, void *lut, s32 fmt, u16 n_entries) {
     tlut_obj->fmt = fmt << 10;
     // The Dolphin SDK doesn't reset the middle bits, which is an issue in Wii mode as they are not
     // masked. Reference (Dolphin Emulator): https://is.gd/ghHrLe
-    tlut_obj->lut = 0x64 << 24 | (reinterpret_cast<uintptr_t>(lut) & 0x1ffffff) >> 5;
+    tlut_obj->lut = 0x64 << 24 | (reinterpret_cast<uintptr_t>(lut) & 0x13ffffff) >> 5;
     tlut_obj->n_entries = n_entries;
 }
--- payload/dolphin/GXTexture.h
+++ payload/dolphin/GXTexture.h
@@ -3,6 +3,13 @@
 #include <common/Types.h>
 #include <payload/Replace.h>
 
+typedef struct GXTexObj {
+    u8 _00[0x0c - 0x00];
+    u32 image_ptr;
+    u8 _10[0x20 - 0x10];
+} GXTexObj;
+size_assert(GXTexObj, 0x20);
+
 typedef struct GXTlutObj {
     s32 fmt;
     u32 lut;
@@ -10,5 +17,10 @@ typedef struct GXTlutObj {
     u8 _a[0xc - 0xa];
 } GXTlutObj;
 size_assert(GXTlutObj, 0xc);
+
+void REPLACED(GXInitTexObj)(GXTexObj *obj, void *image_ptr, u16 width, u16 height, s32 format,
+        s32 wrap_s, s32 wrap_t, BOOL mipmap);
+REPLACE void GXInitTexObj(GXTexObj *obj, void *image_ptr, u16 width, u16 height, s32 format,
+        s32 wrap_s, s32 wrap_t, BOOL mipmap);
 
 REPLACE void GXInitTlutObj(GXTlutObj *tlut_obj, void *lut, s32 fmt, u16 n_entries);
