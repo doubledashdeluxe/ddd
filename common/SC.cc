59fcb6076f6c485eae556fb27f808396a3d712ea
Pablo Stebler <pablo@stebler.xyz>
2024-04-27 21:15:03 +0200

    Refactor SC

--- common/SC.cc
+++ common/SC.cc
@@ -15,3 +15,52 @@ bool SC::ok() const {
     u32 magic = 0x53437630;
     return Bytes::ReadBE<u32>(m_buffer.values(), 0x0) == magic;
 }
+
+bool SC::get(const char *name, u8 &value) {
+    return get(3, name, value);
+}
+
+bool SC::get(const char *name, u16 &value) {
+    return get(4, name, value);
+}
+
+bool SC::get(const char *name, u32 &value) {
+    return get(5, name, value);
+}
+
+bool SC::get(const char *name, u64 &value) {
+    return get(6, name, value);
+}
+
+bool SC::getOffset(u8 type, const char *name, u16 &offset) {
+    if (!ok()) {
+        return false;
+    }
+
+    u16 count = Bytes::ReadBE<u16>(m_buffer.values(), 0x4);
+    for (u16 i = 0; i < count; i++) {
+        offset = 0x6 + i * 0x2;
+        if (offset + sizeof(u16) + sizeof(u8) > m_buffer.count()) {
+            return false;
+        }
+        offset = Bytes::ReadBE<u16>(m_buffer.values(), offset);
+        if (offset + sizeof(u8) > m_buffer.count()) {
+            return false;
+        }
+        u8 lengthAndType = Bytes::ReadBE<u8>(m_buffer.values(), offset);
+        if (lengthAndType >> 5 != type) {
+            continue;
+        }
+        u8 length = (lengthAndType & 0x1f) + 0x1;
+        if (static_cast<size_t>(offset + 0x1 + length) > m_buffer.count()) {
+            return false;
+        }
+        if (memcmp(m_buffer.values() + offset + 0x1, name, length)) {
+            continue;
+        }
+        offset += 0x1 + length;
+        return true;
+    }
+
+    return false;
+}
--- common/SC.hh
+++ common/SC.hh
@@ -9,70 +9,29 @@ extern "C" {
 
 class SC {
 public:
-    template <typename T>
-    struct TypeHelper;
-
     SC();
     ~SC();
     bool ok() const;
-
-    template <typename T>
-    bool get(const char *name, T &value) {
-        if (!ok()) {
-            return false;
-        }
-
-        u16 count = Bytes::ReadBE<u16>(m_buffer.values(), 0x4);
-        for (u16 i = 0; i < count; i++) {
-            u16 offset = 0x6 + i * 0x2;
-            if (offset + sizeof(u16) + sizeof(u8) > m_buffer.count()) {
-                return false;
-            }
-            offset = Bytes::ReadBE<u16>(m_buffer.values(), offset);
-            if (offset + sizeof(u8) > m_buffer.count()) {
-                return false;
-            }
-            u8 lengthAndType = Bytes::ReadBE<u8>(m_buffer.values(), offset);
-            if (lengthAndType >> 5 != TypeHelper<T>::ID) {
-                continue;
-            }
-            u8 length = (lengthAndType & 0x1f) + 0x1;
-            if (static_cast<size_t>(offset + 0x1 + length) > m_buffer.count()) {
-                return false;
-            }
-            if (memcmp(m_buffer.values() + offset + 0x1, name, length)) {
-                continue;
-            }
-            if (offset + 0x1 + length + sizeof(T) > m_buffer.count()) {
-                return false;
-            }
-            value = Bytes::ReadBE<T>(m_buffer.values(), offset + 0x1 + length);
-            return true;
-        }
-
-        return false;
-    }
+    bool get(const char *name, u8 &value);
+    bool get(const char *name, u16 &value);
+    bool get(const char *name, u32 &value);
+    bool get(const char *name, u64 &value);
 
 private:
+    template <typename T>
+    bool get(u8 type, const char *name, T &value) {
+        u16 offset;
+        if (!getOffset(type, name, offset)) {
+            return false;
+        }
+        if (offset + sizeof(T) > m_buffer.count()) {
+            return false;
+        }
+        value = Bytes::ReadBE<T>(m_buffer.values(), offset);
+        return true;
+    }
+
+    bool getOffset(u8 type, const char *name, u16 &offset);
+
     alignas(0x20) Array<u8, 0x4000> m_buffer;
-};
-
-template <>
-struct SC::TypeHelper<u8> {
-    static const u8 ID = 3;
-};
-
-template <>
-struct SC::TypeHelper<u16> {
-    static const u8 ID = 4;
-};
-
-template <>
-struct SC::TypeHelper<u32> {
-    static const u8 ID = 5;
-};
-
-template <>
-struct SC::TypeHelper<u64> {
-    static const u8 ID = 6;
 };
