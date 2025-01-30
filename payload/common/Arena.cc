f36534f9535e1adfd2ef4ab10fd7af73c911ec20
Pablo Stebler <pablo@stebler.xyz>
2025-01-30 23:10:16 +0100

    Add payload MEM1 arena assertions

--- payload/common/Arena.cc
+++ payload/common/Arena.cc
@@ -3,20 +3,24 @@
 #include <common/Align.hh>
 extern "C" {
 #include <dolphin/OSArena.h>
+
+#include <assert.h>
 }
 
 void *MEM1Arena::alloc(size_t size, s32 align) {
+    uintptr_t lo = reinterpret_cast<uintptr_t>(OSGetArenaLo());
+    uintptr_t hi = reinterpret_cast<uintptr_t>(OSGetArenaHi());
     if (align >= 0) {
-        uintptr_t lo = reinterpret_cast<uintptr_t>(OSGetArenaLo());
         lo = AlignUp<uintptr_t>(lo, align);
         void *ptr = reinterpret_cast<void *>(lo);
         lo += size;
+        assert(lo <= hi);
         OSSetArenaLo(reinterpret_cast<void *>(lo));
         return ptr;
     } else {
-        uintptr_t hi = reinterpret_cast<uintptr_t>(OSGetArenaHi());
         hi -= size;
         hi = AlignDown<uintptr_t>(hi, -align);
+        assert(hi >= lo);
         OSSetArenaHi(reinterpret_cast<void *>(hi));
         void *ptr = reinterpret_cast<void *>(hi);
         return ptr;
