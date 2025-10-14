23385ca86f138b080c3da5df797b6a92665abf6f
Pablo Stebler <pablo@stebler.xyz>
2025-10-14 21:14:01 +0200

    Add more Array tests

--- tests/Array.cc
+++ tests/Array.cc
@@ -1,47 +1,81 @@
 #include <portable/Array.hh>
 #include <snitch/snitch_all.hpp>
 
-#include <cstdio>
+#include <cstring>
 
 TEST_CASE("Array") {
-    Array<char, 6> hello;
-    snprintf(hello.values(), hello.count(), "hello");
-    Array<char, 6> world;
-    snprintf(world.values(), world.count(), "world");
+    Array<char, 5> alloy;
+    memcpy(alloy.values(), "alloy", alloy.count());
+    Array<char, 5> loyal;
+    memcpy(loyal.values(), "loyal", loyal.count());
+
+    SECTION("count") {
+        CHECK(alloy.count() == 5);
+    }
+
+    SECTION("values") {
+        CHECK(alloy.values()[4] == 'y');
+        alloy.values()[4] = 'w';
+        CHECK(alloy.values()[4] == 'w');
+    }
+
+    SECTION("operator[]") {
+        CHECK(alloy[4] == 'y');
+        alloy[4] = 'w';
+        CHECK(alloy[4] == 'w');
+    }
+
+    SECTION("rotateLeft") {
+        alloy.rotateLeft(0);
+        CHECK(alloy == alloy);
+        alloy.rotateLeft(5);
+        CHECK(alloy == alloy);
+        alloy.rotateLeft(2);
+        CHECK(alloy == loyal);
+    }
+
+    SECTION("rotateRight") {
+        alloy.rotateRight(0);
+        CHECK(alloy == alloy);
+        alloy.rotateRight(5);
+        CHECK(alloy == alloy);
+        alloy.rotateRight(3);
+        CHECK(alloy == loyal);
+    }
 
     SECTION("operator==") {
-        CHECK(hello == hello);
-        CHECK_FALSE(hello == world);
-        CHECK_FALSE(world == hello);
+        CHECK(alloy == alloy);
+        CHECK_FALSE(alloy == loyal);
+        CHECK_FALSE(loyal == alloy);
     }
 
     SECTION("operator!=") {
-        CHECK_FALSE(hello != hello);
-        CHECK(hello != world);
-        CHECK(world != hello);
+        CHECK_FALSE(alloy != alloy);
+        CHECK(alloy != loyal);
+        CHECK(loyal != alloy);
     }
 
     SECTION("operator<") {
-        CHECK_FALSE(hello < hello);
-        CHECK(hello < world);
-        CHECK_FALSE(world < hello);
+        CHECK_FALSE(alloy < alloy);
+        CHECK(alloy < loyal);
+        CHECK_FALSE(loyal < alloy);
     }
 
     SECTION("operator>") {
-        CHECK_FALSE(hello > hello);
-        CHECK_FALSE(hello > world);
-        CHECK(world > hello);
+        CHECK_FALSE(alloy > alloy);
+        CHECK_FALSE(alloy > loyal);
+        CHECK(loyal > alloy);
     }
 
     SECTION("operator<=") {
-        CHECK(hello <= hello);
-        CHECK(hello <= world);
-        CHECK_FALSE(world <= hello);
+        CHECK(alloy <= alloy);
+        CHECK(alloy <= loyal);
+        CHECK_FALSE(loyal <= alloy);
     }
 
     SECTION("operator>=") {
-        CHECK(hello >= hello);
-        CHECK_FALSE(hello >= world);
-        CHECK(world >= hello);
+        CHECK(alloy >= alloy);
+        CHECK_FALSE(alloy >= loyal);
+        CHECK(loyal >= alloy);
     }
 }
