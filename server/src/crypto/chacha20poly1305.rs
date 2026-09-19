11c0d7138f821d489c3cc8795091a7499ecf4257
Pablo Stebler <pablo@stebler.xyz>
2026-09-19 15:34:43 +0200

    Update orion to 0.18.0

--- server/src/crypto/chacha20poly1305.rs
+++ server/src/crypto/chacha20poly1305.rs
@@ -13,11 +13,12 @@ impl Cipher for ChaCha20Poly1305 {
     }
 
     fn encrypt(k: &Self::Key, nonce: u64, ad: &[u8], plaintext: &[u8], out: &mut [u8]) {
-        let k = SecretKey::from_slice(k.as_slice()).unwrap();
+        let k = SecretKey::try_from(k.as_slice()).unwrap();
         let mut full_nonce = [0u8; 12];
         full_nonce[4..].copy_from_slice(&nonce.to_le_bytes());
         let full_nonce = Nonce::from(full_nonce);
-        chacha20poly1305::seal(&k, &full_nonce, plaintext, Some(ad), out).unwrap();
+        chacha20poly1305::ChaCha20Poly1305::seal(&k, &full_nonce, plaintext, Some(ad), out)
+            .unwrap();
     }
 
     fn encrypt_in_place(_: &Self::Key, _: u64, _: &[u8], _: &mut [u8], _: usize) -> usize {
@@ -31,11 +32,12 @@ impl Cipher for ChaCha20Poly1305 {
         ciphertext: &[u8],
         out: &mut [u8],
     ) -> Result<(), ()> {
-        let k = SecretKey::from_slice(k.as_slice()).unwrap();
+        let k = SecretKey::try_from(k.as_slice()).unwrap();
         let mut full_nonce = [0u8; 12];
         full_nonce[4..].copy_from_slice(&nonce.to_le_bytes());
         let full_nonce = Nonce::from(full_nonce);
-        chacha20poly1305::open(&k, &full_nonce, ciphertext, Some(ad), out).map_err(|_| ())
+        chacha20poly1305::ChaCha20Poly1305::open(&k, &full_nonce, ciphertext, Some(ad), out)
+            .map_err(|_| ())
     }
 
     fn decrypt_in_place(
